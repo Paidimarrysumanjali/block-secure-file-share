f0403c5f988670b389c51b2b335e27a5a19be16b
Sumanjali <paidimarrysuma@gmail.com>
2025-04-15 23:31:44 +0530

    Add secure file sharing code and CI pipeline

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+
 from secure_share.main import generate_key, encrypt_data, decrypt_data
 
 def test_encryption_decryption():
